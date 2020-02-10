86b4139c6e1c80a20040eddb6107e12afa55ef72
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-10 14:44:23 -0500

    Switch from ARNs to profiles for providers

--- providers.tf
+++ providers.tf
@@ -1,18 +1,14 @@
 # This is the "default" provider that is used to create resources
 # inside the Images account
 provider "aws" {
-  region = var.aws_region
-  assume_role {
-    role_arn = "arn:aws:iam::${var.images_account_id}:role/${var.provisionaccount_role_name}"
-  }
+  profile = "cool-images-provisionaccount"
+  region  = var.aws_region
 }
 
 # This is the "users" provider that is used to create resources inside
 # the users account
 provider "aws" {
-  alias  = "users"
-  region = var.aws_region
-  assume_role {
-    role_arn = "arn:aws:iam::${var.users_account_id}:role/${var.provisionaccount_role_name}"
-  }
+  alias   = "users"
+  profile = "cool-users-provisionaccount"
+  region  = var.aws_region
 }
