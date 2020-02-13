5d63eb5ec1b6a0af145ce4844dedc259585e3fa8
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-02-13 14:38:29 -0500

    Update/simplify code for new coarser groups paradigm

--- providers.tf
+++ providers.tf
@@ -4,11 +4,3 @@ provider "aws" {
   profile = "cool-images-provisionaccount"
   region  = var.aws_region
 }
-
-# This is the "users" provider that is used to create resources inside
-# the users account
-provider "aws" {
-  alias   = "users"
-  profile = "cool-users-provisionaccount"
-  region  = var.aws_region
-}
